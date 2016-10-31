74a3636029f42bf98f3556aa8345fc82934f4a64
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-10-31 17:22:55 +0100

    Dodano przykład implementacji StateTriggera do wyświetlenia stanu baterii

---
+++ Altkom.Bicycle.UWPClient/Triggers/BatteryTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.System.Power;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+
+namespace Altkom.Bicycle.UWPClient.Triggers
+{
+    public class BatteryTrigger : StateTriggerBase
+    {
+        public bool Charging { get; set; }
+
+        public BatteryTrigger()
+        {
+            Windows.Devices.Power.Battery.AggregateBattery.ReportUpdated
+                += async (sender, args) => await UpdateStatus();
+
+            Task.Run(()=>UpdateStatus());
+        }
+
+
+        private async Task UpdateStatus()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var batteryReport = Windows.Devices.Power.Battery.AggregateBattery.GetReport();
+
+                switch (batteryReport.Status)
+                {
+                    case BatteryStatus.Idle:
+                        SetActive(!this.Charging);
+                        break;
+
+                    case BatteryStatus.Charging:
+                        SetActive(!this.Charging);
+                        break;
+                    case BatteryStatus.Discharging:
+                        SetActive(this.Charging);
+                        break;
+                }
+
+            });
+        }
+    }
+}
